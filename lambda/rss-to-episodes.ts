60df08dcb8eed5ebbe92c72409a7e26657ec9ffb
Andrew Murphy <andrew@andrewmurphy.io>
2023-06-13 11:58:44 +0100

    Add to SNS

---
+++ lambda/rss-to-episodes.ts
@@ -0,0 +1,71 @@
+import parsePodcast from "node-podcast-parser";
+const request = require("request");
+
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
+
+exports.handler = async function (event: any) {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+  console.log("podcast rss: ", process.env.RSS_FEED_URL);
+  console.log("topic arn: ", process.env.TOPIC_ARN);
+
+  await downloadPodcast(process.env.RSS_FEED_URL!, 10);
+};
+
+function downloadPodcast(url: string, limit: number) {
+  // create AWS SDK clients
+  const sns = new SNSClient();
+  console.log(`sns: ${sns}`);
+
+  return new Promise((resolve, reject) => {
+    console.log(`url: ${url}`);
+    getRSS(url)
+      .then((rss: any) => {
+        console.log(`rss: ${rss}`);
+        const podcast = { ...rss, items: undefined };
+        let downloadEpisodes: any[] = [];
+        let episodes = limit == 0 ? rss.episodes : rss.episodes.slice(0, limit);
+        console.log(`episodes: ${episodes}`);
+        const podcastPromises = episodes.map((episode: any) => {
+          console.log(`episode: ${episode.title}:${episode.enclosure.url}`);
+          const publishCommand = new PublishCommand({
+            TopicArn: process.env.TOPIC_ARN,
+            Message: JSON.stringify({
+              podcast,
+              episode,
+            }),
+          });
+          return sns
+            .send(publishCommand)
+            .then((publishResult) => {
+              console.log(`Message sent. The ID is ${publishResult.MessageId}`);
+              downloadEpisodes.push(podcast);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        });
+
+        return Promise.all(podcastPromises).then(() => downloadEpisodes);
+      })
+      .then((downloadEpisodes) => {
+        resolve(downloadEpisodes);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function getRSS(url: string) {
+  return new Promise((resolve, reject) => {
+    request(url, (err: string, res: any, data: any) => {
+      if (err) reject(`Network error: ${err}`);
+
+      parsePodcast(data, (err: any, data: any) => {
+        if (err) reject(`Parsing error: ${err}`);
+
+        resolve(data);
+      });
+    });
+  });
+}
--- lib/podcast-download-and-transcriber-stack.ts
+++ lib/podcast-download-and-transcriber-stack.ts
@@ -2,13 +2,13 @@ import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { HitCounter } from './hitcounter';
 import { TableViewer } from 'cdk-dynamo-table-viewer';
-
-
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class PodcastDownloadAndTranscriberStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -42,7 +42,32 @@ export class PodcastDownloadAndTranscriberStack extends Stack {
       title: 'Hello Hits',
       table: helloWithCounter.table,
       sortBy: '-hits'       // optional ("-" denotes descending order),
-      
     });
+
+
+    // defines an AWS Lambda resource
+    const rssToEpisodes = new NodejsFunction(this, 'RSS-To-Episodes', {
+      bundling: {
+        externalModules: [],
+        minify: false,
+        nodeModules: ['node-podcast-parser', '@aws-sdk/client-sns'],
+      },
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: __dirname + '/../lambda/rss-to-episodes.ts',
+      handler: 'handler',
+      timeout: Duration.seconds(360),
+      environment: {
+        RSS_FEED_URL: "https://anchor.fm/s/fa79b44/podcast/rss",
+        TOPIC_ARN: topic.topicArn
+      }
+    });
+
+    const snsTopicPolicy = new iam.PolicyStatement({
+      actions: ['sns:publish'],
+      resources: ['*'],
+    });
+
+    rssToEpisodes.addToRolePolicy(snsTopicPolicy);
   }
+
 }
